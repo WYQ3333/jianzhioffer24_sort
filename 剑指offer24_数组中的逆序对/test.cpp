a152d3ae3194f1bab9ee95fbebd55799ba3e8495
浅忆梦微凉 <3538922590@qq.com>
2019-02-03 21:29:31 +0800

    归并排序（不懂）

--- 剑指offer24_数组中的逆序对/test.cpp
+++ 剑指offer24_数组中的逆序对/test.cpp
@@ -29,29 +29,77 @@ public:
 	}
 };
 
-class Solution {
+class Solution2 {
 public:
-	int InversePairs(vector<int> data) {
-		int result = 0;
+	int num(vector<int> data, int p){
 		int index = 0;
-		int temp = data[0];
+		int temp = data[p];
 		int i = 0;
-		for (i = 1; i < data.size(); ++i){
+		for (i = p+1; i < data.size(); ++i){
 			if (data[i] < temp)
 				++index;
 		}
+		return index;
+	}
+	int InversePairs(vector<int> data) {
+		int result = 0;
+		int i = 0;
+		int index = num(data, 0);
 		result = index;
-		i = 1;
 		for (i = 1; i < data.size(); ++i){
-			if (data[i]>temp){
-				result += index;
-				temp = data[i];
-			}
-			else if (data[i] < temp){
-				--index;
-			}
+			index = num(data, i);
+			result += index;
 		}
 		return result % 1000000007;
+	}
+};
+
+class Solution {
+public:
+	int InversePairs(vector<int> data) {
+		int length = data.size();
+		if (length <= 0)
+			return 0;
+		//vector<int> copy=new vector<int>[length];
+		vector<int> copy;
+		for (int i = 0; i<length; i++)
+			copy.push_back(data[i]);
+		long long count = InversePairsCore(data, copy, 0, length - 1);
+		//delete[]copy;
+		return count % 1000000007;
+	}
+	long long InversePairsCore(vector<int> &data, vector<int> &copy, int start, int end)
+	{
+		if (start == end)
+		{
+			copy[start] = data[start];
+			return 0;
+		}
+		int length = (end - start) / 2;
+		long long left = InversePairsCore(copy, data, start, start + length);
+		long long right = InversePairsCore(copy, data, start + length + 1, end);
+
+		int i = start + length;
+		int j = end;
+		int indexcopy = end;
+		long long count = 0;
+		while (i >= start&&j >= start + length + 1)
+		{
+			if (data[i]>data[j])
+			{
+				copy[indexcopy--] = data[i--];
+				count = count + j - start - length;          //count=count+j-(start+length+1)+1;
+			}
+			else
+			{
+				copy[indexcopy--] = data[j--];
+			}
+		}
+		for (; i >= start; i--)
+			copy[indexcopy--] = data[i];
+		for (; j >= start + length + 1; j--)
+			copy[indexcopy--] = data[j];
+		return left + right + count;
 	}
 };
 
